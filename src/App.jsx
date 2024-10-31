cdcbd36cc250b5933a87922431d85fca97a2b638
Mayur Kamble <mayurkamble0250@gmail.com>
2024-10-31 09:55:12 +0530

    added pages and route them in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import Home from "./pages/Home";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+import ViewBook from "./pages/ViewBook";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="/books/create" elements={<CreateBook />} />
+      <Route path="/books/edit/:id" elements={<EditBook />} />
+      <Route path="/books/delete/:id" elements={<DeleteBook />} />
+      <Route path="/books/details/:id" elements={<ViewBook />} />
+      <Route path="/" elements={<Home />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
